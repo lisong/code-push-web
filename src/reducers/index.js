776fc308859f18e3c7ceebd46bc28f9f32cec92d
tablee <lisong2010@gmail.com>
2016-08-08 16:21:34 +0800

    logout clear state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,6 @@
 import { combineReducers } from 'redux';
+import {USER_LOGOUT} from '../actions/actionTypes';
+import _ from 'lodash';
 import {users, login} from './users';
 import {auth, accessKeys} from './auth';
 import {routes} from './routes';
@@ -14,6 +16,14 @@ const appReducer = combineReducers({
 });
 
 const rootReducer = (state, action) => {
+  if (action.type === USER_LOGOUT) {
+    let data = Object.assign({}, state);
+    _.set(data, 'auth', {});
+    _.set(data, 'users', {});
+    _.set(data, 'products', {});
+    _.set(data, 'accessKeys', {});
+    return appReducer(data, action)
+  }
   return appReducer(state, action)
 };
 
