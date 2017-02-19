2ba4f7f95d7eb89c615df9bab802c09c911101cf
tablee <lisong2010@gmail.com>
2017-02-19 11:35:57 +0800

    update framework

---
+++ src/containers/LayoutContainer.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import _ from 'lodash';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Layout from '../components/Layout';
+
+class LayoutContainer extends Component {
+  render() {
+    const {auth, actions } = this.props;
+    return (
+      <Layout>
+        <Header
+          isAuth={_.get(auth, 'isAuth')}
+          />
+        {this.props.children}
+        <Footer/>
+      </Layout>
+    );
+  }
+}
+
+function mapStateToProps(state, ownProps) {
+  return {'auth': _.get(state, 'auth', {})};
+}
+
+export default connect(
+  mapStateToProps
+)(LayoutContainer)
