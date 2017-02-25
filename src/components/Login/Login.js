ce4e0976a2e284eebaecc80e12c52a5a152fb013
tablee <lisong2010@gmail.com>
2017-02-25 23:20:07 +0800

    fix login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,5 @@
 import React, { PropTypes, Component } from 'react';
 import _ from 'lodash';
-import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import {
   Col,
   ControlLabel,
@@ -54,15 +53,15 @@ class Login extends Component {
 
   render() {
     return (
-      <Form style={{ height:600 }} horizontal>
+      <Form style={{ height:600, paddingLeft: 20, paddingRight:20 }} horizontal>
         <br/>
         <FormGroup>
-          <Col smOffset={3}>
+          <Col smOffset={2}>
             <h1>请登录</h1>
           </Col>
         </FormGroup>
         <FormGroup controlId="formHorizontalEmail">
-          <Col componentClass={ControlLabel} sm={3}>
+          <Col componentClass={ControlLabel} sm={2}>
             邮箱地址
           </Col>
           <Col sm={4}>
@@ -77,7 +76,7 @@ class Login extends Component {
         </FormGroup>
 
         <FormGroup controlId="formHorizontalPassword">
-          <Col componentClass={ControlLabel} sm={3}>
+          <Col componentClass={ControlLabel} sm={2}>
             登录密码
           </Col>
           <Col sm={4}>
@@ -90,12 +89,12 @@ class Login extends Component {
           </Col>
         </FormGroup>
         <FormGroup>
-          <Col smOffset={3} sm={4} style={{ color:'red' }} >
+          <Col smOffset={2} sm={4} style={{ color:'red' }} >
           {_.get(this.props, 'error.errorMessage')}
           </Col>
         </FormGroup>
         <FormGroup>
-          <Col smOffset={3} sm={2}>
+          <Col smOffset={2} sm={2}>
             <Button
               onClick={this.submit}
               disabled={this.props.isFetching}
@@ -105,7 +104,7 @@ class Login extends Component {
           </Col>
         </FormGroup>
         <FormGroup>
-          <Col smOffset={3} sm={4}>
+          <Col smOffset={2} sm={4}>
             <span style={{ marginRight:"20px" }}>还没有账号?</span>
             <Link to="/register">立即注册</Link>
           </Col>
